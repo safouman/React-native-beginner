66937800dc7fc3974453747c1448342fc30bbc58
safouman <safouman@gmail.com>
2017-09-28 14:42:19 +0100

    react-native first commit

---
+++ index.android.js
@@ -0,0 +1,23 @@
+//Place code in here for android
+
+
+//import a library to help create a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+
+// create a Component
+const App = () => (
+
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+
+  );
+
+
+// render it to device
+ AppRegistry.registerComponent('albums', () => App);
